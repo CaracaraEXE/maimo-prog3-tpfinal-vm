803af9ff4d4df4e36695f67045d00b2e0835a075
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-11-11 23:13:11 -0300

    better conditional rendering

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -14,16 +14,22 @@ const NavBar = () => {
             <p className='bg-red-800 text-white text-[0.75em] p-2'><span className='font-bold'>¡NO A LOS CUD FALSOS!</span> <Link href={"/alerta"} className='underline'>Lee nuestra declaración completa</Link></p>
         </section>
     <section className='bg-gray-600 p-2 flex justify-between px-4'>
-      <button onClick={() => {!nav ? setNav(true) : setNav(false)}}>Menu</button>
+      
+      {!nav ?
+      <button onClick={() => {setNav(true)}}>Menu</button>
+    :  
+          <button onClick={() => {setNav(false)}}>Salir</button>
+
+    }
       <span className='text-3xl'>VJ</span>
     </section>
     {nav &&
-    <nav className='p-4 list-none bg-gray-500'>
-      <li className='mb-2 text-2xl'>Test</li>
-       <li className='mb-2 text-2xl'>Test</li>
-       <li className='mb-2 text-2xl'>Test</li>
-        <li className='text-2xl'>Test</li>
-        
+    <nav className='p-4 pl-8 list-none bg-gray-500'>
+      <li className='mb-3 text-2xl'>Eventos</li>
+      <li className='mb-3 text-2xl'>Lugares</li>
+      <li className='mb-3 text-2xl'>Calendario</li>
+      <li className='mb-3 text-2xl'>Sobre Nosotros</li>
+      <li className='text-2xl'>¡NO A LOS CUD FALSOS!</li>
     </nav>
     }
     </div>
