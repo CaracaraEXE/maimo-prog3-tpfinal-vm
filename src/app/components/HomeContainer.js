d4716d063a293c1fc9f65e855aeb2545aea0cb5e
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-11-12 23:44:00 -0300

    made homecontainer responsive

--- src/app/components/HomeContainer.js
+++ src/app/components/HomeContainer.js
@@ -4,26 +4,30 @@ import Footer from './Footer'
 
 const HomeContainer = () => {
   return (
-    <>
+    <div className='flex flex-col justify-center'>
     <section className='static'>
       <Image
         src={'/assets/deer.png'}
         alt={'header'}
         height={100}
         width={500}
+        className='lg:w-full lg:h-80'
       />
     </section>
 
-    <p className='px-2 text-center'>Creado por y para personas con discapacidades.</p>
+    <p className='px-2 text-center py-5'>Creado por y para personas con discapacidades.</p>
     
-    <section className='flex flex-col h-60 lg:flex-row'>
+    <section className='flex flex-col h-60 lg:h-50 lg:flex-row'>
       <button className='bg-orange-700 h-full w-full text-2xl'>LUGARES</button>
       <button className='bg-teal-700 h-full w-full text-2xl'>Eventos</button>
     </section>
 
-    <button className='bg-gray-700 p-2 px-4 rounded-3xl'>O sino buscá por Barrio! </button>
+    <p className='text-center mt-5'>VER POR MAPA:</p>
+
+
+    <button className='bg-gray-700 p-2 px-4 rounded-3xl w-fit m-auto my-4 text-[1.25em] lg:px-10 lg:text-2xl'>O sino buscá por Barrio! </button>
     
-    </>
+    </div>
   )
 }
 
