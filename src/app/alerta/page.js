83d3e093893f9026d3fa9342a5935d554aa3dd6b
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-11-15 17:36:55 -0300

    continuing the pages

---
+++ src/app/alerta/page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const pagePSA = () => {
+  return (
+    <div>
+        <h1>¡NO A LOS CUDS FALSOS!</h1>
+        <p>Todos los beneficios mencionados en esta pagina son por y para <span className='font-bold'>personas con discapacidad. Vamos Juntos se opone totalmente a la falsificación de Certificados Únicos de Discapacidad a modo de obtener "ventajas" sobre otros.</span></p>
+
+        <h2>¿Qué está pasando?</h2>
+        <p>Desde el año 2019 se han estado reportando casos de gente falsificando este tipo de certificados, como por ejemplo 13 denunciados que colocaron el simbolo de discapacidad a modo de engañar sobre su condicion y obtener beneficios exclusivos.</p>
+        <p>No obstante, esta problematica ha regresado con mas fuerza estos ultimos años, donde empresas de colectivos y choferes por igual han reportado incidentes con personas poseyentes de un CUD falso a modo de obtener un modo gratuito de trasnporte.</p>
+        <p>Estas denuncias llevaron a una investigación a fondo, donde se descubrió una red de compra y venta ilegal de CUDs falsos en plataformas como Facebook Marketplace. También juega un lugar muy imporante la situación economica del pais, en donde el aumento del transporte publico ha hecho que mas personas usesn estos metodos de acceso a modo de acceso.</p>
+
+        <h2>¿Por que es importante hablar de esto?</h2>
+        <p>Las consecuencias de la falsificacion de CUDS AFECTA A LAS VERDADERAS PERSONAS CON DISCAPACIDAD. Por ejemplo, muchos colectivos están cambiando su forma de ofrecer el pase, haciendo que una persona discapactiada tenga que mostrar 2 documentos diferentes solamente para poder ingresar al vehículo, lo cual puede otorgar mas complejidad a la hora de viajar.</p>
+        <p>Además, la falsificación de los Certificados Unicos de Discapacidad pueden afectar severamente el proceso de registro de los mismos, haciendo muy dificil poder conseguirlo para personas quienes lo necesiten.</p>
+        <p>En la sociedad argentina, se ha estado circulando un sentimiento de "discapacidados falsos", que mientras puede tener fundamentos, su mal uso puede causar prejuicios muy negativos para los 1,3 millones de personas que poseen un CUD en la Argentina.</p>
+        
+        <section>
+            <p>El CUD no es....</p>
+                <ul>
+                    <li>Un pase gratis</li>
+                    <li>Un metodo de transporte gratis</li>
+                </ul>
+        </section>
+    </div>
+  )
+}
+
+export default pagePSA
